ff8a6840c85ad0a35a1245fd7beb792cbdda0e94
John Dunning <john_dunning@yahoo.com>
2018-11-01 18:20:09 -0700

    Get all HAR when RequestList mounts
    Add key to list items.
    Add styled-components.

--- src/js/RequestList.js
+++ src/js/RequestList.js
@@ -1,10 +1,17 @@
 import React from "react";
+import styled from "styled-components";
 import parse from "date-fns/parse";
 import format from "date-fns/format";
 
 
 const StartTimeFormat = "HH:mm:ss.SSS";
 const StartDateFormat = "YYYY-MM-DD";
+
+
+const Timestamp = styled.span`
+    font-size: 12px !important;
+    font-family: Consolas, Lucida Console, Courier New, monospace;
+`;
 
 
 function processHAR(
@@ -32,6 +39,11 @@ export default class RequestList extends React.Component{
 
 	componentWillUnmount()
 	{
+console.log("mount");
+		chrome.devtools.network.getHAR(log => {
+console.log(log.entries.length);
+			this.setState({ requests: log.entries.map(har => processHAR(har)) });
+		});
 		chrome.devtools.network.onRequestFinished.removeListener(this.handleRequestFinished);
 	}
 
@@ -49,8 +61,11 @@ export default class RequestList extends React.Component{
 
 	render()
 	{
-		const items = this.state.requests.map(request => <li
-        title={request.startDateString}>{request.startTimeString}</li>);
+		const items = this.state.requests.map((request, i) => (
+			<li key={i}>
+				<Timestamp title={request.startDateString}>{request.startTimeString}</Timestamp>
+			</li>
+		));
 
 		return (
 			<ul>
